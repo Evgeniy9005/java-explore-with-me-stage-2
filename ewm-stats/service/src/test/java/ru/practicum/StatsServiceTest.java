8eb91abcc3fc89a8ad74dcb4ae92c58cb6dba142
Евгений <Evgeniy_90_05@mail.ru>
2024-06-26 13:59:18 +1100

    Тест SpringBootTest начало

--- ewm-stats/service/src/test/java/ru/practicum/StatsServiceTest.java
+++ ewm-stats/service/src/test/java/ru/practicum/StatsServiceTest.java
@@ -1,8 +1,42 @@
 package ru.practicum;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.model.EndpointHitMapper;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class StatsServiceTest {
+
+    @Autowired
+    private StatsService statsService;
+
+    @Autowired
+    private EndpointHitMapper hitMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @BeforeAll
+    static void data() {
+
+
+    }
+
+@BeforeEach
+void satrt(){
+
+    }
 
 }
