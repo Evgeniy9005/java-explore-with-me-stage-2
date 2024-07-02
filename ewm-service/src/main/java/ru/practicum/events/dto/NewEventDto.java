6af00ab1a268ab52ecccc1df3d3ed5c015d97db6
Evgeniy9005 <evgeniy_90_05@mail.ru>
2024-07-02 15:34:04 +1100

    Часть теста для EventsController. Тест для метода addEventUser

--- ewm-service/src/main/java/ru/practicum/events/dto/NewEventDto.java
+++ ewm-service/src/main/java/ru/practicum/events/dto/NewEventDto.java
@@ -10,8 +10,13 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Builder(toBuilder = true)
 @RequiredArgsConstructor
 public class NewEventDto {
+
+    /**
+     Краткое описание события
+     */
     @NotBlank
     @Max(2000)
     @Min(20)
@@ -21,12 +26,18 @@ public class NewEventDto {
     maxLength: 2000
     minLength: 20
     example: Сплав на байдарках похож на полет.
-    Краткое описание события*/
-
+    */
+    /**
+     id категории к которой относится событие
+     */
     private final int category;
    /* category*	integer($int64)
     example: 2
-    id категории к которой относится событие*/
+    */
+
+    /**
+     Полное описание события
+     */
     @Max(7000)
     @Min(20)
     private final String description;
@@ -35,35 +46,51 @@ public class NewEventDto {
     minLength: 20
     example: Сплав на байдарках похож на полет. На спокойной воде — это парение. На бурной, порожистой — выполнение фигур высшего пилотажа. И то, и другое дарят чувство обновления, феерические эмоции, яркие впечатления.
     Полное описание события*/
-
+    /**
+     Дата и время на которые намечено событие. Дата и время указываются в формате "yyyy-MM-dd HH:mm:ss"
+     */
     private final String eventDate;
     /*eventDate*	string
     example: 2024-12-31 15:10:05
-    Дата и время на которые намечено событие. Дата и время указываются в формате "yyyy-MM-dd HH:mm:ss"
 */
+    /**
+     Широта и долгота места проведения события
+     */
     private final Location location;
     /*location*	Location{...}
     Jump to path*/
+    /**
+     Нужно ли оплачивать участие в событии
+     */
     @Builder.Default
     private final boolean paid = false;
     /*paid	boolean
     example: true
     default: false
-    Нужно ли оплачивать участие в событии*/
+    */
+    /**
+     Ограничение на количество участников. Значение 0 - означает отсутствие ограничения
+     */
     @Builder.Default
     private final int participantLimit = 0;
    /* participantLimit	integer($int32)
     example: 10
     default: 0
-    Ограничение на количество участников. Значение 0 - означает отсутствие ограничения*/
+    */
+    /**
+     Нужна ли пре-модерация заявок на участие. Если true,
+     то все заявки будут ожидать подтверждения инициатором события.
+     Если false - то будут подтверждаться автоматически
+     */
     @Builder.Default
     private final boolean requestModeration = true;
     /*requestModeration	boolean
     example: false
     default: true
-    Нужна ли пре-модерация заявок на участие. Если true,
-    то все заявки будут ожидать подтверждения инициатором события.
-    Если false - то будут подтверждаться автоматически.*/
+    */
+    /**
+     Заголовок события
+     */
     @Max(120)
     @Min(3)
     private final String title;
@@ -71,6 +98,6 @@ public class NewEventDto {
     maxLength: 120
     minLength: 3
     example: Сплав на байдарках
-    Заголовок события*/
+   */
 
 }
--- ewm-service/src/test/java/ru/practicum/events/EventsControllerTest.java
+++ ewm-service/src/test/java/ru/practicum/events/EventsControllerTest.java
@@ -1,18 +1,31 @@
 package ru.practicum.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import ru.practicum.client.StatsClient;
 import ru.practicum.events.dto.NewEventDto;
+import ru.practicum.events.model.Location;
+import ru.practicum.stats.Stats;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = EventsController.class)
@@ -24,10 +37,27 @@ class EventsControllerTest {
     @MockBean
     private EventsService eventsService;
 
+    @Mock
+    private StatsClient statsClient;
+
     @Autowired
     private MockMvc mvc;
 
     private NewEventDto newEventDto;
+
+    @BeforeEach
+    void start() {
+    newEventDto = NewEventDto.builder()
+            .annotation("Краткое описание события")
+            .paid(false)
+            .location(new Location(53.1F,54.2F))
+            .eventDate("2024-12-31 15:10:05")
+            .description("Полное описание события")
+            .participantLimit(0)
+            .requestModeration(true)
+            .title("Событие")
+            .build();
+    }
 
     @Test
     void getEvents() {
@@ -39,13 +69,28 @@ class EventsControllerTest {
     }
 
     @Test
-    void addEventUser() {
-        mvc.perform(post("/users/1/events")
-                        .content(objectMapper.writeValueAsString(endpointHitDto))
-                        .characterEncoding(StandardCharsets.UTF_8)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+    void addEventUser() throws Exception {
+       // HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+
+        try (MockedStatic<Stats> theMock = Mockito.mockStatic(Stats.class)) {
+            theMock.when(Stats::getStatsClient).thenReturn(statsClient);
+
+            when(statsClient.put(any())).thenReturn("Ответ");
+            mvc.perform(post("/users/1/events")
+                            .with(request -> {
+                                request.setRemoteAddr("192.168.0.1");
+                                return request;
+                            })
+                            .content(objectMapper.writeValueAsString(newEventDto))
+                            .characterEncoding(StandardCharsets.UTF_8)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isCreated());
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 
